332b62f2a6c536204f46ee6e94e34c9c0eeaffc3
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2025-10-17 15:16:31 -0400

    flutter

--- interactive_noise.py
+++ interactive_noise.py
@@ -3,8 +3,9 @@ from matplotlib.animation import FuncAnimation
 import math
 import time
 import colorsys
+from random import uniform
 
-# region
+
 
 
 def get_points_on_circle(center, radius, num_points):
@@ -22,6 +23,7 @@ def clamp(value, min_val=0.0, max_val=1.0):
     """Clamp a number between min_val and max_val."""
     return max(min_val, min(max_val, value))
 
+
 def get_color_indices(value, num_colors):
     """
     Given a normalized value [0,1], find the two color indices
@@ -36,12 +38,23 @@ def get_color_indices(value, num_colors):
 
 
 def lerp(a, b, t):
-    """Linear interpolation between a and b by t."""
+    """Linear interpolation between a and b by normalized value t."""
     return a * (1 - t) + b * t
+
 
 def inverse_lerp(a, b, x):
     """ Inverse Interpolation to normalize x by a and b"""
     return (x - a) / (b - a)
+
+
+def nudge(current, target, up_speed, down_speed):
+    """ Eases or nudges the current toward the target by factor speed """
+    if current < target:
+        return min(current + up_speed, target)
+    elif current > target:
+        return max(current - down_speed, target)
+    return current
+
 
 def height_to_hue(height, hues):
     """
@@ -84,14 +97,33 @@ def get_height(x, y, scale):
 
 def get_distance():
     """ generates a sin betwixt 0 and 10 if distance, the unit is meters """
-    #return (math.sin(t/2) + 1) * 5
+    # return (math.sin(t/2) + 1) * 5
     return mouse_x
+
+
+def refresh_values():
+    # nudge values toward target
+    # set new targets
+    for i, value in enumerate(values):
+        values[i] = nudge(value, target_values[i], flutter_speed_up, flutter_speed_down)
+        if uniform(0, 1) < flutter_probability: # add random flutters
+            target_values[i] = 1.0
+        if value == target_values[i]:
+            target_values[i] = MIN_V
 
 
 def set_hsv(frame):
     """ Set the LEDS """
+    global flutter_probability, flutter_speed_up, flutter_speed_down
     offset = frame * 0.05
-    scale = clamp(inverse_lerp(-11, 11, get_distance()), 0, 1)
+    scale = clamp(inverse_lerp(-11, 11, get_distance()), 0, 1) # normalized distance
+
+    flutter_probability = lerp(.2, .02, scale)
+    flutter_speed_up = lerp(.15, .02, scale)
+    flutter_speed_down = lerp(.15, .02, scale)
+    print("flutter:", flutter_probability, flutter_speed_up, flutter_speed_down)
+
+    refresh_values()
 
     for (x, y), i in points:
         # Add time offset for flowing noise3
@@ -99,50 +131,60 @@ def set_hsv(frame):
         height = get_height(x + offset, y + offset * 0.3, scale)
         # Map noise to leds
         hue = height_to_hue(height, hue_palette)
-        sv_scale = lerp(.75, 1, 1 - scale) # normalize and invert value for hsv
+        # normalize and invert value for hsv
+        sv_scale = lerp(.75, 1, 1 - scale)
         saturation = sv_scale
-        value = sv_scale
-        hsv = (hue, saturation, value)
-        print("scales", scale, sv_scale)
+        hsv = (hue, saturation, values[i])
+        # print("scales", scale, sv_scale)
         # print('hsv', hsv)
         LEDS[i] = (hsv)
 
     # set dots with converted hsv
     dots.set_facecolors([colorsys.hsv_to_rgb(*hsv) for hsv in LEDS])
+    time.sleep(.01)
     return [dots]
+
 
 NUM_LEDS = 120
 LEDS = [(0, 0, 0)] * NUM_LEDS  # preallocate memory
 print(LEDS)
 
+flutter_probability = .02
+flutter_speed_up = .05
+flutter_speed_down = .2
+MIN_V = .38
+values = [MIN_V] * NUM_LEDS
+target_values = [MIN_V] * NUM_LEDS # the target to lerp towards
+
+
 # Get evenly spaced points along all curves
 points = get_points_on_circle((0, 0), 10, NUM_LEDS)
 print(points)
 
-hue_palette = [0, .01, .1, .2, .3]
+hue_palette = [0, .05, .1, .2, .3]
 # hue_palette = [.3, .38, .4, .58, .62]
 
+
+# region replace plotting callbacks with LED loop
 # Plotting
 fig, ax = plt.subplots()
 px, py = zip(*[p for p, _ in points])  # px = all x values, py = all y values
 dots = ax.scatter(px, py, c='r', s=80)
 
-#region mouse movement
 mouse_x = 0
 mouse_y = 0
+
 def on_mouse_move(event):
     global mouse_x, mouse_y
     if event.inaxes:  # Only track when inside the plot
         mouse_x, mouse_y = event.xdata, event.ydata
-        #print("Mouse at data coords:", mouse_x, mouse_y)
+        # print("Mouse at data coords:", mouse_x, mouse_y)
+
 
 # Connect the motion event to the handler
 cid = fig.canvas.mpl_connect('motion_notify_event', on_mouse_move)
-#endregion
+
 
 ani = FuncAnimation(fig, set_hsv, frames=1000, interval=50, blit=False)
 plt.show()
-time.sleep(.2)
-
-
-
+# endregion
